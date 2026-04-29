75838455ec4de9155a9c4ce88c8ba49a81e00107
najeebsg78-ship-it <najeebsg78@gmail.com>
2026-04-30 00:26:36 +0200

    gjorde account model och book model

--- src/main/java/org/example/fulkopinginlamning/model/Account.java
+++ src/main/java/org/example/fulkopinginlamning/model/Account.java
@@ -1,4 +1,100 @@
 package org.example.fulkopinginlamning.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table (name = "DBAccount")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer authorId;
+
+    @Column (nullable = false)
+    private String firstname;
+    private String lastname;
+    private String country;
+    private String address;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date changed;
+
+    public Account(){
+        created = new Date();
+        changed = new Date();
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getChanged() {
+        return changed;
+    }
+
+    public void setChanged(Date changed) {
+        this.changed = changed;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "authorId=" + authorId +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", country='" + country + '\'' +
+                ", address='" + address + '\'' +
+                ", created=" + created +
+                ", changed=" + changed +
+                '}';
+    }
 }
--- src/main/java/org/example/fulkopinginlamning/model/Book.java
+++ src/main/java/org/example/fulkopinginlamning/model/Book.java
@@ -1,4 +1,61 @@
 package org.example.fulkopinginlamning.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DBBook")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookId;
+
+    @Column(nullable = false)
+    private String title;
+    private String author;
+    private String category;
+
+    public Book(){};
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
